3df050192297516aa2c39d4cd4ff8e36f033bc5e
callmeashish <ashishkumarsingh046@gmail.com>
2019-11-14 23:40:18 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+  return 'Hello World!\n'
+
+"""Dynamic route"""
+@app.route('/hello/<username>') 
+def hello_user(username):
+  return 'Why Hello %s!\n' % username
+
+if __name__ == '__main__':
+  
+  """Debug Mode"""
+  app.run(debug=True)
